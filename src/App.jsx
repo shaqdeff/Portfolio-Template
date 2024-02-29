b2516b2a8861a959230546750cceea8daeb65dd4
Cooper Harris <cwharris365@gmail.com>
2024-02-29 12:49:11 -0700

    Added analytics libraries from Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+// Vercel Analytics
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
+
 import { BrowserRouter } from 'react-router-dom';
 import {
   About,
